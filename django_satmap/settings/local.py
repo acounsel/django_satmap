a3ffd4701e49588f416bf16743c81c8787f429de
melissahliu <mliu1785@stanford.edu>
2023-08-31 12:18:35 -0700

    sql lite

--- django_satmap/settings/local.py
+++ django_satmap/settings/local.py
@@ -6,14 +6,21 @@ ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.ngrok.io', 'localhost']
 
 INSTALLED_APPS += ['debug_toolbar']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'satmap',
+#         'USER': 'samer',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'satmap',
-        'USER': 'samer',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
